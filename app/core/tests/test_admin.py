e7c5ebbb8f98a413906317d42cef5d6a5ca1ec83
Nilot-21 <nilotpaldas211@gmail.com>
2024-07-04 12:24:16 +0530

    working with user api

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+"""
+Test for the django admin modifications
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTest(TestCase):
+    """Test for django admin"""
+    def setUp(self):
+       """create user and client."""
+       self.client = Client()
+       self.admin_user = get_user_model().objects.create_superuser(
+           email='admin@example.com',
+           password='admin',
+       )
+       self.client.force_login(self.admin_user)
+       self.user = get_user_model().objects.create_user(
+           email='user@example.com',
+           password='testpass123',
+           name='Test user',
+        #    phone_no='1234567890',
+       )
+
+    def test_user_list(self):
+        """Test that users are listed """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+        # self.assertContains(res, self.user.phone_no)
+
+    def test_create_user_page(self):
+        """Test create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
